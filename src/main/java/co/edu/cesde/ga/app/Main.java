b2dd5c72a84c7ecdc407a8be9de8bcf2a69a5176
luisar0602 <luisar0602@gmail.com>
2026-02-21 13:28:46 -0500

    proyecto base, solo Main con Maven

--- src/main/java/co/edu/cesde/ga/app/Main.java
+++ src/main/java/co/edu/cesde/ga/app/Main.java
@@ -1,2 +1,7 @@
-package co.edu.cesde.ga.app;public class Main {
+package co.edu.cesde.ga.app;
+
+public class Main {
+    public static void main (String[] args ) {
+        System.out.println("hello World!");
+    }
 }
